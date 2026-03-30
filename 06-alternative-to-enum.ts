8901dfc3abdb9b394ba5608c03031b2743b67530
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2026-03-30 22:48:51 +0800

    Done playing and updating the 06-alternative-to-enum.ts file

--- 06-alternative-to-enum.ts
+++ 06-alternative-to-enum.ts
@@ -1,11 +1,23 @@
 (() => {
     // Literal Types (Alternative to Enum)
-    let userRole: 'admin' | 'guess' | 'teacher' | 'student' = 'admin'
+    let userRole: 'admin' | 'guest' | 'teacher' | 'student' = 'student'
 
-    userRole = 'guess'
-    // anotherUserRole = 'superadmin'
+    // userRole = 'guest'
+    
+    // let anotherUserRole: UserRole = 'superadmin'
 
-    function access(role: 'admin' | 'guess' | 'teacher' | 'student') {
-        // ...
+    function access(role: 'admin' | 'guest' | 'teacher' | 'student') {
+        if (role === 'admin') {
+            return ['read', 'write', 'delete']
+        } else if (role === 'teacher') {
+            return ['read', 'write']
+        } else {
+            return ['read']
+        }
     }
-})
+
+    console.log(userRole)
+    // console.log(anotherUserRole)
+
+    access('guest')
+})()
