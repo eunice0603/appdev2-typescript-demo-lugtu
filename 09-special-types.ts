985d5045e1ff1fbbd810157bf759b943a78cfde0
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2026-03-30 23:00:10 +0800

    Done playing and updating the 09-special-types.ts file

--- 09-special-types.ts
+++ 09-special-types.ts
@@ -1,25 +1,31 @@
 // Special Type - null
 let a: null;
 
-a = null
-// a = "Hi"
+// a = null
+a = "Hello?"
 
 let b: null | string
 
-b = null
-b = "Hi"
-// b = 10
+// b = null
+// b = "Hello!"
+b = 10.89
+
+console.log(a)
+console.log(b)
 
 // Special Type - undefined
 let c: undefined;
 
 c = undefined
 // c = null
-// c = "Hi"
+// c = "Hello."
 
 let d: undefined | string
 
 d = undefined
-d = "Hi"
+d = "Hello..."
 // d = null
 // d = 10
+
+console.log(c)
+console.log(d)
